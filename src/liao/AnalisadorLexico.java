e24d208307d727a6cbb540d3d8bdd5cede1f64ad
mateus <mateus.loures@gmail.com>
2017-03-28 14:31:28 -0300

    erro com string _ adicionado

--- src/liao/AnalisadorLexico.java
+++ src/liao/AnalisadorLexico.java
@@ -136,9 +136,10 @@ public class AnalisadorLexico {
                     } else if ( isDigito(c) || isLetra(c) ) { // muda para e7 se n tiver mais '_' no id
                         lexema+=c;
                         estadoAtual = 7;
-                    }else
+                    }else{
                         System.out.println("ERRO id apenas com _");
-                    
+                        return  ;
+                    }
                     break;
                     
                 case 7: // leu primeiro caracater do id ou leu '_'
